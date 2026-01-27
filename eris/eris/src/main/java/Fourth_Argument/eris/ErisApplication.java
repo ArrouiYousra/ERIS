12ede5c13fc6df4a5db1f7eaca3b60baa8583f81
Alexandre Leroy <alexandre.leroy@epitech.eu>
2026-01-27 14:29:11 +0100

    build : Rebuild with Spring Initializr

--- eris/eris/src/main/java/Fourth_Argument/eris/ErisApplication.java
+++ eris/eris/src/main/java/Fourth_Argument/eris/ErisApplication.java
@@ -1,4 +1,4 @@
-package Fourth.Argument.eris;
+package Fourth_Argument.eris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
