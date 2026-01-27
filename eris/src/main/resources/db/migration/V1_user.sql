7fa4274a3f7f0666a52277068685ffab4e77055c
Alexandre Leroy <alexandre.leroy@epitech.eu>
2026-01-27 14:14:41 +0100

    Create db + spring pom.xml

---
+++ eris/src/main/resources/db/migration/V1_user.sql
@@ -0,0 +1,15 @@
+CREATE TYPE statut AS ENUM ('online','offline','absent')
+
+
+CREATE TABLE IF NOT EXISTS user (
+    id                  serial primary key,
+    username            varchar(255),
+    email               varchar(255),
+    password            varchar(255),
+    display_name        varchar(255),
+    current_statut      statut,
+    created_at          timestamp
+    updated_at          timestamp,
+
+
+);
