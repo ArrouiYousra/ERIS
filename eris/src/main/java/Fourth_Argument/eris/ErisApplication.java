8b017289bbeb8730e5903b67dd360c0e387b0d59
Wejmaime <paul.lasjunies@epitech.eu>
2026-01-27 15:14:49 +0100

    added a littel hello wolrd function on prot 8080, and deleted all database init to compil

--- eris/src/main/java/Fourth_Argument/eris/ErisApplication.java
+++ eris/src/main/java/Fourth_Argument/eris/ErisApplication.java
@@ -2,12 +2,20 @@ package Fourth_Argument.eris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ErisApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(ErisApplication.class, args);
+		SpringApplication.run(ErisApplication.class, args);  // Changé ici !
 	}
 
 }
