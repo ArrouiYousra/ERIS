ba4372ebf5aceeb6f8d115b807001f6849050485
Wejmaime <paul.lasjunies@epitech.eu>
2026-01-28 15:23:05 +0100

    edit : cleaning code

---
+++ backend/src/main/java/Fourth_Argument/eris/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,16 @@
+package Fourth_Argument.eris.messagingstompwebsocket;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ backend/src/main/java/Fourth_Argument/eris/messagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,20 @@
+package Fourth_Argument.eris.messagingstompwebsocket;
+
+public class HelloMessage {
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
