7cbd6660da1c1170dde10132e9bb9c6f183794fe
martalex31 <alexandre1.martinez@epitech.eu>
2026-02-02 17:32:23 +0100

    feat: exceptions

---
+++ backend/src/main/java/Fourth_Argument/exceptions/ChannelException.java
@@ -0,0 +1,8 @@
+package Fourth_Argument.exceptions;
+
+public class ChannelException extends Exception {
+
+    public ChannelException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/Fourth_Argument/exceptions/ServerException.java
@@ -0,0 +1,8 @@
+package Fourth_Argument.exceptions;
+
+public class ServerException extends Exception {
+
+    public ServerException(String message) {
+        super(message);
+    }
+}
