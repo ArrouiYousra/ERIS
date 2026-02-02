b303a94b8c9d1be39d3065c017d32f3ba1a17cdc
Alexandre Leroy <alexandre.leroy@epitech.eu>
2026-02-02 13:47:11 +0100

    build: Role Entity

---
+++ backend/src/main/java/Fourth_Argument/eris/api/model/Permissions.java
@@ -0,0 +1,12 @@
+package Fourth_Argument.eris.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "permission")
+public class Permissions {
+
+}
