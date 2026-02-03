f550e3d736b33cfb37202f81ba77a4e569eac5e2
Yousra Arroui <yousra.arroui@epitech.eu>
2026-02-03 14:00:21 +0100

    feat: testing auth

--- backend/src/main/java/Fourth_Argument/eris/api/security/WebSecurityConfig.java
+++ backend/src/main/java/Fourth_Argument/eris/api/security/WebSecurityConfig.java
@@ -11,14 +11,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+// On peut déclarer des Bean qui sont créés au démarrage et injectés dans les autres classes
 @Configuration
+// Active la sécurité web Spring, sans ça, les filtres et règles de sécurité ne seront pas appliquées
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
-    private final AuthEntryPointJwt unauthorizedHandler;
-    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService; // Filtre utilisateur pour vérifier les identifiants
+    private final AuthEntryPointJwt unauthorizedHandler; // Requête non authentifiée (401)
+    private final JwtUtil jwtUtil; // Validation du token JWT
 
+    // On injecte les beans dans le constructeur
     public WebSecurityConfig(CustomUserDetailsService userDetailsService,
                              AuthEntryPointJwt unauthorizedHandler,
                              JwtUtil jwtUtil) {
@@ -27,28 +30,37 @@ public class WebSecurityConfig {
         this.jwtUtil = jwtUtil;
     }
 
+    // Filtre pour vérifier le token JWT
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter(jwtUtil, userDetailsService);
     }
 
+    // Gestionnaire d'authentification
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
 
+    // Filtre de sécurité
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Désactive la protection CSRF (Cross-Site Request Forgery)
                 .csrf(csrf -> csrf.disable())
+                // Désactive la gestion des CORS (Cross-Origin Resource Sharing)
                 .cors(cors -> cors.disable())
+                // Gère les erreurs d'authentification
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                // Désactive la gestion des sessions stateless http
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // On définit les règles d'authorisation (public/authentifié)
                 .authorizeHttpRequests(auth -> auth
+                        // pas besoin de token ici
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().authenticated()
-                );
+                        // doit etre authentifier
+                        .anyRequest().authenticated());
+        // On ajoute le filtre JWT
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
