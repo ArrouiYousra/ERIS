fcfc24e70b42bbfbf3899f9d8504796875e9f6f7
Alexandre Leroy <alexandre.leroy@epitech.eu>
2026-02-06 13:18:55 +0100

    fix: auth logic

---
+++ backend/src/main/java/Fourth_Argument/eris/api/dto/response/LoginResponseDTO.java
@@ -0,0 +1,18 @@
+package Fourth_Argument.eris.api.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponseDTO {
+
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
+}
