a5d20cffcaa6815755af0c7b0cb3dbc6763e5911
Alexandre Leroy <alexandre.leroy@epitech.eu>
2026-03-11 18:17:27 +0100

    build: started edit messages

--- frontend/src/api/messageApi.ts
+++ frontend/src/api/messageApi.ts
@@ -11,3 +11,8 @@ export async function getMessageByChannel(channelId: number) {
   const { data } = await api.get(`/api/channels/${channelId}/messages`);
   return data;
 }
+
+export async function deleteMessage(messageId: number) {
+  const { data } = await api.delete(`/messages/${messageId}/`);
+  return data;
+}
