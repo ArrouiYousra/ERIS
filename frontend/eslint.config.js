b3d5386e53cbf16a182f9c3be10cc44227f94817
Wejmaime <paul.lasjunies@epitech.eu>
2026-03-13 15:05:17 +0100

    debug : correcting _ issue and adding globalignore (['dist', '.vite', 'coverage'])

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,10 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-{ ignores: ['.vite/**', 'coverage/**'] }
-
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.vite', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,6 +18,13 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      }],
     },
   },
 ])
